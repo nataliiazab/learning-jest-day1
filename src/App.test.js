afc40113315391939f23c0c858efc1c28d03892d
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-06-18 18:00:36 +0100

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,6 @@
+import {render, screen, waitFor} from '@testing-library/react';
+import user from '@testing-library/user-event';
+import App from './App';
+
+test('shows 6 products by default', () => {};)
+
